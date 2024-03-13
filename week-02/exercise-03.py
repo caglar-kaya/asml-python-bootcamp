7c5da116478f19d3550c4b60c1957605f3301d48
Caglar Kaya <c.kaya@intermax.nl>
2024-03-13 19:15:08 +0100

    feat: week 02 variables and casting

---
+++ week-02/exercise-03.py
@@ -0,0 +1,17 @@
+my_integer = int("5")
+print(my_integer) # 5
+
+my_float = float("5")
+print(my_float) # 5.0
+
+my_string = str(5.45)
+print(my_float) # 5.0
+
+my_string_from_bool = str(True)
+print(my_string_from_bool) # True
+
+my_bool_from_string = bool("True")
+print(my_string_from_bool) # True
+
+my_integer_from_bool = int(False)
+print(my_integer_from_bool) # 0
